ebb59b0016ac36351f97e7ce1eb3859501942695
Landon Rohatensky <landonroha@gmail.com>
2017-03-18 09:14:43 -0600

    cocoapods init with firebase

---
+++ booklog/booklog/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  booklog
+//
+//  Created by Landon Rohatensky on 2017-03-18.
+//  Copyright © 2017 tastytreats. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
